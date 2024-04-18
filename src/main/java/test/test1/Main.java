6a18ad4289d8d38ac83a6e1da7aa48b9814f1807
sparklep <sparklep519@gmail.com>
2024-04-18 12:11:57 +0900

    beakjoon

--- src/main/java/test/test1/Main.java
+++ src/main/java/test/test1/Main.java
@@ -1,7 +1,26 @@
 package test.test1;
 
+import java.io.*;
+import java.util.*;
+/**
+ * 사용된 개념과 접근법, 어떻게 풀지에 대해 서술하고 시작한다.
+ --------------------------------------------------
+ * 이 클래스는 사용자가 지정한 최대 값까지 소수를 구한다.
+ * 알고리즘은 에라스토테네스 체다.
+ * 2에서 시작하는 정수 배열을 대상으로 작업한다.
+ * 처음으로 남아 있는 정수를 찾아 배수를 모두 제거한다.
+ * 배열에 더 이상 배수가 없을 때까지 반복한다.
+ --------------------------------------------------
+
+ * 그리고 각 핵심코드에 1)의도를 설명하는 주석 2)의미를 명료하게 밝히는 주석을 적습니다.
+ 아래 코드 참고
+
+ */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("test2");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
     }
 }
